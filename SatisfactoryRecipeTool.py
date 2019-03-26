7dd6aa2cfb1a21f3670f1e7c32db02b6c7e76eeb
Nicolas Broillet <nicolas.broillet@hevs.ch>
2019-03-26 11:48:45 +0100

    First idea for Recipes

---
+++ SatisfactoryRecipeTool.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 11:40:48 2019
+
+@author: Nicolas Broillet
+@version : 0.1
+"""
+
+#ironOre = Recipe('ironOre', 1, [])
+
+#ironIngot = Recipe('ironIngot', 1, [Quantity(ironOre, 1)])
+
+#ironPlate = Recipe('ironPlate', 1, [Quantity(ironIngot, 2)])
+
+#screw = Receipe('screw', 6, [Quantity(ironRod, 1)])
+
+def main():
+    print('Hello World')
+
+main()

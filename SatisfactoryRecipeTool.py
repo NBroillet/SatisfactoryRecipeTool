f556ec6128587ff41b59c1d24460df9fe718c70b
Nicolas Broillet <nicolas.broillet@hevs.ch>
2019-03-26 12:48:29 +0100

    Add Quantity

--- SatisfactoryRecipeTool.py
+++ SatisfactoryRecipeTool.py
@@ -6,16 +6,50 @@ Created on Tue Mar 26 11:40:48 2019
 @author: Nicolas Broillet
 @version : 0.1
 """
+# Supposition : smelter at 60 items/min
 
-#ironOre = Recipe('ironOre', 1, [])
+class Recipe:
+    def __init__(self, name, outputPerMin, listRequired):
+        self.name = name
+        self.outputPerMin = outputPerMin
+        self.listRequired = listRequired
+        
+class Quantity:
+    def __init__(self, recipe, inputPerMin):
+        self.recipe = recipe
+        self.inputPerMin = inputPerMin
 
-#ironIngot = Recipe('ironIngot', 1, [Quantity(ironOre, 1)])
+#------------------------------------------------------------------------------
+# Tier 0
+#------------------------------------------------------------------------------
+ironOre = Recipe('ironOre', 60, [])
 
-#ironPlate = Recipe('ironPlate', 1, [Quantity(ironIngot, 2)])
+#------------------------------------------------------------------------------
+# Tier 1
+#------------------------------------------------------------------------------
+ironIngot = Recipe('ironIngot', 60, [Quantity(ironOre, 60)])
 
-#screw = Receipe('screw', 6, [Quantity(ironRod, 1)])
+#------------------------------------------------------------------------------
+# Tier 2
+#------------------------------------------------------------------------------
+ironPlate = Recipe('ironPlate', 15, [Quantity(ironIngot, 30)])
+
+ironRod = Recipe('ironRod', 15, [Quantity(ironIngot, 15)])
+
+#------------------------------------------------------------------------------
+# Tier 3
+#------------------------------------------------------------------------------
+screw = Recipe('screw', 90, [Quantity(ironRod, 15)])
+
+
+#------------------------------------------------------------------------------
+# Tier 4
+#------------------------------------------------------------------------------
+reinforcedIronPlate = Recipe('reinforcedIronPlate', 5, [Quantity(ironRod, 120), Quantity(ironPlate, 20)])
+
+#------------------------------------------------------------------------------
 
 def main():
-    print('Hello World')
+    print()
 
 main()

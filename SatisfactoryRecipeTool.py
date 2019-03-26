935228ee98d719c773fd746e919d1f300adc267e
Nicolas Broillet <nicolas.broillet@hevs.ch>
2019-03-26 13:07:38 +0100

    Add Copper

--- SatisfactoryRecipeTool.py
+++ SatisfactoryRecipeTool.py
@@ -22,30 +22,40 @@ class Quantity:
 #------------------------------------------------------------------------------
 # Tier 0
 #------------------------------------------------------------------------------
-ironOre = Recipe('ironOre', 60, [])
+ironOre = Recipe('iron ore', 60, [])
+copperOre = Recipe('copper ore', 60, [])
+limestoneOre = Recipe('limestone ore', 60, [])
 
 #------------------------------------------------------------------------------
 # Tier 1
 #------------------------------------------------------------------------------
-ironIngot = Recipe('ironIngot', 60, [Quantity(ironOre, 60)])
+ironIngot = Recipe('iron ingot', 30, [Quantity(ironOre, 30)])
+copperIngot = Recipe('copper ingot', 30, [Quantity(copperOre, 30)])
+concrete = Recipe('concrete', 15, [Quantity(limestoneOre, 45)])
 
 #------------------------------------------------------------------------------
 # Tier 2
 #------------------------------------------------------------------------------
-ironPlate = Recipe('ironPlate', 15, [Quantity(ironIngot, 30)])
-
-ironRod = Recipe('ironRod', 15, [Quantity(ironIngot, 15)])
+ironPlate = Recipe('iron plate', 15, [Quantity(ironIngot, 30)])
+ironRod = Recipe('iron rod', 15, [Quantity(ironIngot, 15)])
+wire = Recipe('wire', 45, [Quantity(copperIngot, 15)])
 
 #------------------------------------------------------------------------------
 # Tier 3
 #------------------------------------------------------------------------------
 screw = Recipe('screw', 90, [Quantity(ironRod, 15)])
-
+cable = Recipe('cable', 15, [Quantity(wire, 30)])
 
 #------------------------------------------------------------------------------
 # Tier 4
 #------------------------------------------------------------------------------
-reinforcedIronPlate = Recipe('reinforcedIronPlate', 5, [Quantity(ironRod, 120), Quantity(ironPlate, 20)])
+reinforcedIronPlate = Recipe('reinforced iron plate', 5, [Quantity(ironRod, 120), Quantity(ironPlate, 20)])
+rotor = Recipe('rotor', 6, [Quantity(ironRod, 18), Quantity(screw, 22)])
+
+#------------------------------------------------------------------------------
+# Tier 4
+#------------------------------------------------------------------------------
+modularFrame = Recipe('modular frame', 4, [Quantity(reinforcedIronPlate, 12), Quantity(ironRod, 6)])
 
 #------------------------------------------------------------------------------
 
